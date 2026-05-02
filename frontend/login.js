9673ae1dbbc7dfd5534df57d8df0705abb5815f9
Ayush Tiwari <ayushtiwari24512@gmail.com>
2026-05-02 17:31:29 +0530

    deployed latest version on render

--- frontend/login.js
+++ frontend/login.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://inventory-manager-fs9t.onrender.com";
 
 async function handle_register(e) {
 
